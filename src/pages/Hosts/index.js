0d08cafd6c20d71613af260b9d2b3a2c88dce28b
UdithIndrakantha <udith.indrakantha@gmail.com>
2024-03-25 14:05:03 +0530

    Testnet enabled

--- src/pages/Hosts/index.js
+++ src/pages/Hosts/index.js
@@ -132,7 +132,7 @@ export default function Hosts() {
     <PageTitle
       titleHeading="Hosts"
       />
-      {true ? (<p className="m-0">Host directory data is temporarily unavailable.</p>):
+      {false ? (<p className="m-0">Host directory data is temporarily unavailable.</p>):
       (
       <div>
       {isHostsLoading && <Loader className={`hostsLoader "p-4"`} />}
